5c81f1a14e1bfa966b47a0ce7162317cd3ee54be
Gabriel Molin <molinguin@gmail.com>
2020-12-14 15:26:59 -0300

    add arquivos via upload

---
+++ data-index.js
@@ -0,0 +1,19 @@
+import data0 from './files/[1997]Transpiração_Contínua_Prolongada/data.js';
+import data1 from "./files/[2004]Tamo_ai_na_Atividade/data.js"
+
+var dataIndex = new Array(data0, data1)
+export default dataIndex;
+
+import {lerPlaylist} from './programa.js'
+
+if(document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', pronto)
+}else{pronto()}
+  
+function pronto(){
+    for(var i = 0; i < dataIndex.length; i++) {
+        var indiceDataIndex = dataIndex[i]
+        lerPlaylist(indiceDataIndex, i)
+    }; 
+}
+
--- programa.js
+++ programa.js
@@ -1,50 +1,49 @@
-function montarPagina(){
+function montarPagina(dir){
     var containerConteudo = document.createElement('div')
     containerConteudo.classList.add('container-titulos')
     
     var paginaConteudo = `
-        <div class="capa-album-main" style="background : url('${data[0].cover}') no-repeat center center / cover"></div>
+        <div class="capa-album-main" style="background : url('${dir[0].cover}') no-repeat center center / cover"></div>
         <div class="titulo-aba-container">
-            <h1 class="titulo-aba">${data[0].album}</h1>
-            <h4 class="autor-conteudo">${data[0].artist}</h4>
+            <h1 class="titulo-aba">${dir[0].album}</h1>
+            <h4 class="autor-conteudo">${dir[0].artist}</h4>
         </div>
     `
 
     containerConteudo.innerHTML = paginaConteudo
     areaSuperior.append(containerConteudo)
+
+    addLista(dir)
 }
 
-function addLista(){
-    for (var i = 1; i < data.length; i++) {
-        var posData = data[i]
-        posData.addLista
+function addLista(dir){
+    for (var i = 1; i < dir.length; i++) {
+        var posData = dir[i]
 
         var itemLista = document.createElement('div')
         itemLista.classList.add('item-lista')
         
         
         var itemNovoLista = `
-                <div onclick="selecionarMusica('${posData.title}', '${data[0].artist}', '${data[0].cover}')">
+                <div>
                 <h2>-</h2>
                 <h2 class="item-lista-titulo">${posData.title}</h2>
-                <h2 class="item-lista-album">${data[0].album}</h2>
-                <h2 class="item-lista-artista">${data[0].artist}</h2>
+                <h2 class="item-lista-album">${dir[0].album}</h2>
+                <h2 class="item-lista-artista">${dir[0].artist}</h2>
                 <h2 class="item-lista-dur">--</h2>
                 </div>
             `
         itemLista.innerHTML = itemNovoLista
         areaItens.append(itemLista)
+
     };
 };
 
-function selecionarMusica(){
-    audio.src = posData.file
-};
-
-function lerPlaylists(){
+function lerPlaylist(dir){
     var itemPlaylist = document.createElement('li')
-    itemPlaylist.innerText = `${data[0].album}`
-
+    itemPlaylist.innerHTML = `<div><h3>${dir[0].album}</h3></div>`
+    
+    itemPlaylist.addEventListener('click',() => {montarPagina(dir)})
     playlistsLista.append(itemPlaylist)
 };
 
@@ -54,3 +53,4 @@ function selecionarMusica(nome, artista, cover){
     capaAlbumTocador.style.background = `url("${cover}") no-repeat center center / cover`
 };
 
+export {lerPlaylist};
