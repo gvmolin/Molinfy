bbfcdf262edcb50ccc5d0d4b92f019514114371c
gvmolin <molinguin@gmail.com>
2020-12-15 16:23:25 -0300

    programa principal funcionando, adicionando onscroll events

--- js/programa.js
+++ js/programa.js
@@ -1,22 +1,40 @@
 function montarPagina(dir){
+    areaSuperior.innerHTML = ""
     var containerConteudo = document.createElement('div')
-    containerConteudo.classList.add('container-titulos')
+    containerConteudo.classList.add('aba-titulos')
     
     var paginaConteudo = `
-        <div class="capa-album-main" style="background : url('${dir[0].cover}') no-repeat center center / cover"></div>
-        <div class="titulo-aba-container">
-            <h1 class="titulo-aba">${dir[0].album}</h1>
-            <h4 class="autor-conteudo">${dir[0].artist}</h4>
+        <div class="container-titulos">
+            <div class="capa-album-main" style="background : url('${dir[0].cover}') no-repeat center center / cover"></div>
+            <div class="titulo-aba-container">
+                <h1 class="titulo-aba">${dir[0].album}</h1>
+                <h4 class="autor-conteudo">${dir[0].artist}</h4>
+            </div>
         </div>
-    `
+
+        <div class="label-lista">
+            <h2>#</h2>
+            <h2>TITULO</h2>
+            <h2>ALBUM</h2>
+            <h2>ARTISTA</h2>
+            <h2>DURACAO</h2>
+        </div>
+        <hr style="width: 98%;"/>
+        `
 
     containerConteudo.innerHTML = paginaConteudo
     areaSuperior.append(containerConteudo)
-
     addLista(dir)
+
+    /*tituloPagina.innerHTML = dir[0].album
+    autorPagina.innerHTML = dir[0].artist
+    capaAlbumAba.style.background = `url('${dir[0].cover}') no-repeat center center / cover`
+    console.log(dir[0].album)
+    addLista(dir)*/
 }
 
 function addLista(dir){
+    areaItens.innerHTML = ""
     for (var i = 1; i < dir.length; i++) {
         var posData = dir[i]
 
